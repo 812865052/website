332c2ad1c652bfaf5a03763f8e539032aaca1bcf
feiye(叶飞) <feiye@tencent.com>
2018-03-03 15:30:06 +0800

    test form

--- mysite/invest/views.py
+++ mysite/invest/views.py
@@ -14,6 +14,8 @@ from django.views import generic
 import json
 from django.core import serializers
 
+from .forms import NameForm
+
 def index(request):
     company_list = sharePrice.objects.all()
     # time.strftime('%Y-%m-%d', time.strptime("30 Nov 17", "%d %b %y"))
@@ -23,6 +25,32 @@ def index(request):
         'company_list': data,
     }
     return render(request, 'invest/index.html', context)
+
+# def test(request):
+#     context = {
+#         #'company_list': json.dumps(company_list),
+#         'company_list': data,
+#     }
+#     return render(request, 'invest/index.html', context)
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            # return HttpResponseRedirect('/index/')
+            return render(request, 'index_temp.html', {'form': form})
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'index.html', {'form': form})
 
 # def detail(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
