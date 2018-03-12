d64adedcea5417d63315d5c57878d6b016b00a02
feiye(叶飞) <feiye@tencent.com>
2018-03-13 01:00:02 +0800

    0 queryset return 0

--- mysite/invest/db.py
+++ mysite/invest/db.py
@@ -73,6 +73,8 @@ def db_queryprice(path, table, company, date):
     number = cur.execute("select * from %s where company=? and data=?" % (table), (company,date,))
     print number
     temp = number.fetchone()
+    if(temp==None):
+        return 0
     close(cur,conn)
     print temp
     print 'price'+ temp[3]
@@ -139,14 +141,10 @@ def insertdate(year,month,day,data,companylist):
     return data
 
 def insertcompany(path,data,table,companylist):
-    print data
     for i in data:
-        print i
         for company in companylist:
-            print company,type(company)
             # db_queryprice(path, table, company, date)
             i[company] = db_queryprice(path, table, company, i['date'])
-    print 'wz' + data
     return data
 
 
