5554531dfd5273d6b9ba5c74831c9a32f4dabced
feiye(叶飞) <feiye@tencent.com>
2018-03-13 13:10:11 +0800

    log add

--- mysite/invest/views.py
+++ mysite/invest/views.py
@@ -87,36 +87,14 @@ def compare(request):
             # ...
             # redirect to a new URL:
             # return HttpResponseRedirect('/index/')
-            return render(request, 'invest/index_temp.html', {'form': companylist})
+            context = {
+                'data' = data,
+                'form': companylist,
+            }
+            return render(request, 'invest/index_temp.html', context)
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = compareCompany()
 
     return render(request, 'index.html', {'form': form})
-
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
-
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': question})
-
-# def vote(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     try:
-#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-#     except (KeyError, Choice.DoesNotExist):
-#     # Redisplay the question voting form.
-#         return render(request, 'polls/detail.html', {
-#             'question': question,
-#             'error_message': "You didn't select a choice.",
-#         })
-#     else:
-#         selected_choice.votes += 1
-#         selected_choice.save()
-#         # Always return an HttpResponseRedirect after successfully dealing
-#         # with POST data. This prevents data from being posted twice if a
-#         # user hits the Back button.
-#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
