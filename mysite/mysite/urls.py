5794b40ab36be8f84f2a847581d8d4c521c61779
feiye(叶飞) <feiye@tencent.com>
2018-03-21 00:45:41 +0800

    views import add

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
+from invest import views
 
 urlpatterns = [
     url(r'^invest/', include('invest.urls')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^upload_script/$', 'invest.views.uploadify_script', name='uploadify_script'),
-    url(r'^delete_uploadfile/$', 'invest.views.file_delete', name='file_delete'),
+    url(r'^upload_script/$', views.uploadify_script, name='uploadify_script'),
+    url(r'^delete_uploadfile/$', invest.views.file_delete, name='file_delete'),
 ]
