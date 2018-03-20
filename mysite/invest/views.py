53174ed24157cd42277af42e076340a0b5be0be3
feiye(叶飞) <feiye@tencent.com>
2018-03-21 00:59:51 +0800

    path fix

--- mysite/invest/views.py
+++ mysite/invest/views.py
@@ -18,7 +18,7 @@ from db import db_query, insertdate, insertcompany, db_insert, db_delete, db_del
 from .forms import addCompanyData, compareCompany, deleteCompanyData, deleteCompanyidData
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
-static_path = '\\home\\website\\demo\\mysite\\invest\\static\\upload'
+static_path = '/home/website/demo/mysite/invest/static/upload'
 
 def index(request):
     company_list = sharePrice.objects.all()
