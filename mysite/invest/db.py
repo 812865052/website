532582629473a319c1b89f430ec17415e0653327
feiye(叶飞) <feiye@tencent.com>
2018-03-21 22:47:12 +0800

    bug fix

--- mysite/invest/db.py
+++ mysite/invest/db.py
@@ -227,7 +227,7 @@ def returndbdate(path,table):
     conn = connect(path)
     cur=conn.cursor()
     t = (table)
-    number = cur.execute("select * from %s order by id" %t) #by id desc 逆序
+    number = cur.execute("select * from %s order by id" %t) #by id desc
     temp = number.fetchall()
     print temp
     i = 0
