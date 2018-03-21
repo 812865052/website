7cf812f45eae7e9eae0bb3b2e82dc63e9a1c9558
feiye(叶飞) <feiye@tencent.com>
2018-03-21 22:40:33 +0800

    bug fix

--- mysite/invest/db.py
+++ mysite/invest/db.py
@@ -237,16 +237,14 @@ def returndbdate(path,table):
         #print row[1]
         if (temp[i][1] in list):
             print temp[i][1]
-            print temp[i][1].encode('ascii','ignore')
             print list
             print 'in'
             i = i + 1
         else:
             print temp[i][1]
-            print temp[i][1].encode('ascii','ignore')
             print list
             print 'not in'
-            list.append(temp[i][1]).encode('ascii','ignore')
+            list.append(temp[i][1].encode('ascii','ignore'))
             i = i + 1
         
     close(cur,conn)
