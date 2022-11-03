0b57171ada309144cd3450481a71e964186cd35a
yefei-shopee-mbp16 <yefei@shopee.com>
2022-11-03 17:07:54 +0800

    加入viewset

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from invest import views
-from snippets import views as snippets_views
 
 urlpatterns = [
     url(r'^invest/', include('invest.urls')),
--- mysite/snippets/views.py
+++ mysite/snippets/views.py
@@ -5,46 +5,37 @@ from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import renderers
+from rest_framework import viewsets
 
 
-class SnippetList(generics.ListCreateAPIView):
-    # 当我们的类视图继承了GenericAPIview，必须在定义的方法前，指定查询结果集和所要使用的序列化器
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    此视图自动提供`list`，`create`，`retrieve`，`update`和`destroy`操作。
+
+    另外我们还提供了一个额外的`highlight`操作。
+    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                      IsOwnerOrReadOnly,)
-    # temp = SnippetSerializer(queryset[0])
-    
-    # print(f"queryset:{queryset[0].created}")
-    # # print(f"temp:{temp}")
-    # print(f"serializer_class.data:{serializer_class.data}")
-    # print(f"serializer_class:{serializer_class}")
-    # # print(f"serializer_class属性:{dir(serializer_class)}")
-    # print(f"serializer_class.fields:{serializer_class.get_fields()}")
-    
+                          IsOwnerOrReadOnly,)
+
+    @action(detail=True, methods=['post'])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-        print(serializer)
-    
-
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                      IsOwnerOrReadOnly,)
     
     
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    此视图自动提供`list`和`detail`操作。
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     
@@ -55,12 +46,3 @@ def api_root(request, format=None):
         'users': reverse('user-list', request=request, format=format),
         'snippets': reverse('snippet-list', request=request, format=format)
     })
-    
-    
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    renderer_classes = (renderers.StaticHTMLRenderer,)
-
-    def get(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
